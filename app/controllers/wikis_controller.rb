973ee19a5ef95f052e28ca529b8338b3ea82f002
SaintClair33 <ceo.wonu@gmail.com>
2015-09-05 15:34:38 -0400

    Added CRUD and respective views for Wiki pages

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,5 +1,6 @@
 class WikisController < ApplicationController
   before_action :find_wiki, only: [:show, :edit, :update, :destroy]
+
   def index
     @wikis = Wiki.all
   end
@@ -13,7 +14,8 @@ class WikisController < ApplicationController
 
   def create
     @wiki = Wiki.new(wiki_params)
-    @wiki.user = current_user
+
+    @wiki.user_id = current_user
     
     if @wiki.save
       flash[:notice] = "Your wiki has been saved."
@@ -22,20 +24,27 @@ class WikisController < ApplicationController
       flash[:error] = "Something went wrong."
       render :new
     end
-
-
   end
 
   def edit
   end
 
   def update
-    
+    #@wiki.user_id = current_user
+    if @wiki.update_attributes(wiki_params)
+      flash[:notice] = "Wiki is updated"
+      redirect_to @wiki
+    else 
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :edit
+    end
   end
 
   def destroy
+
     if @wiki.destroy
-      redirect_to wikis_path, notice: "Wiki deleted successfully"
+      flash[:notice] = "Wiki deleted successfully"
+      redirect_to wikis_path
     else
       flash[:error] = "Something went wrong"
       render :show
